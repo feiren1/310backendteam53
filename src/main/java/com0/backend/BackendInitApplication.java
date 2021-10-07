0f743c7615da6cb23da6d64a574710de74d3a24c
Ryan Li <ryanli@usc.edu>
2021-10-06 17:17:27 -0700

    new initializr

---
+++ src/main/java/com0/backend/BackendInitApplication.java
@@ -0,0 +1,13 @@
+package com0.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendInitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendInitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com0/backend/BackendInitApplicationTests.java
@@ -0,0 +1,13 @@
+package com0.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendInitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
