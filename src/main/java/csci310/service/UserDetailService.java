8ef33aee291f5d5f004ca365ea9976769a59ce54
Fei Ren <feir@usc.edu>
2021-10-20 18:38:57 -0700

    pushed updates

--- src/main/java/csci310/service/UserDetailService.java
+++ src/main/java/csci310/service/UserDetailService.java
@@ -10,10 +10,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class UserDetailService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
